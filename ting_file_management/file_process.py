252488f7856a0b116acc2093f5dba7af4b86f998
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-15 17:18:10 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    text = txt_importer(path_file)
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text
+    }
+
+    instance.enqueue(file)
+    sys.stdout.write(str(file))
+    return file
 
 
 def remove(instance):
