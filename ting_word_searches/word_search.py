b851eb71b643dd9f308f992e4a2c9daed250a0f8
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-15 18:34:22 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for index, content in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in content.lower():
+                result["ocorrencias"].append({"linha": index + 1})
+
+        if result["ocorrencias"]:
+            response.append(result)
+
+    return response
 
 
 def search_by_word(word, instance):
