1f066a39edf91eac122ce1d27d478ef8a1143125
Cláudio Melgaço <claudio.melgaco@ape11.com.br>
2023-06-15 16:29:20 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+            content_split = content.split("\n")
+        return content_split
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
